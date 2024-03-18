8a96cb77e0d4db60eea8652c7e9c526e787853f3
Quent-Repo <grrkekisbest@gmail.com>
2024-03-18 16:45:15 -0400

    Gui
    Added in tk and am using the 'pack' layout.

--- Click.py
+++ Click.py
@@ -1,14 +1,58 @@
 import pyautogui
-import tkinter
+from tkinter import *
 import keyboard
 import time
 print(pyautogui.size())
 count = 0
+
+
+
+app = Tk()
+app.title("Nova clicker")
+app.geometry("800x800")
+
+
+def start():
+    while True:
+        if keyboard.is_pressed("q"):
+            pyautogui.click(clicks= 50, interval=.0001)
+        if keyboard.is_pressed("w"):
+            break
+
+
+
+
+
+
+#Number of click
+number_clicks = Frame(app)
+number_clicks.pack()
+Label(number_clicks, text="Number of clicks").pack(side="left",padx=5)
+number_clicks_entry = Entry(number_clicks)
+number_clicks_entry.pack(side="right")
+
+
+
+#interval time
+number_interval = Frame(app)
+number_interval.pack()
+Label(number_interval, text="Time interval").pack(side="left",padx=5)
+number_interval_entry = Entry(number_interval)
+number_interval_entry.pack(side="right")
+
+funer= Button(app, text="Start", command=start, width=10)
+funer.pack()
+
+app.mainloop()
+
+
+'''
 while True:
     if keyboard.is_pressed("q"):
-        pyautogui.click(clicks= 20, interval=.0001)
+        pyautogui.click(clicks= 50, interval=.0001)
         count+=1
     if keyboard.is_pressed("w"):
         break
 
 print(count)
+'''
