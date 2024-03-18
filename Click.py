9a3392542bfa37f55b032520a42a61529f12ef4b
Quent-Repo <grrkekisbest@gmail.com>
2024-03-18 17:31:48 -0400

    Ui update
    Added in working ui.

--- Click.py
+++ Click.py
@@ -12,17 +12,20 @@ app.title("Nova clicker")
 app.geometry("800x800")
 
 
-def start():
+def start(the_cli=50,the_int=.0001):
     while True:
         if keyboard.is_pressed("q"):
-            pyautogui.click(clicks= 50, interval=.0001)
+            pyautogui.click(clicks= the_cli, interval=the_int)
         if keyboard.is_pressed("w"):
             break
 
 
 
 
-
+def go():
+    arg= int(number_clicks_entry.get())
+    arg2= float(number_interval_entry.get())
+    start(arg,arg2)
 
 #Number of click
 number_clicks = Frame(app)
@@ -40,7 +43,7 @@ Label(number_interval, text="Time interval").pack(side="left",padx=5)
 number_interval_entry = Entry(number_interval)
 number_interval_entry.pack(side="right")
 
-funer= Button(app, text="Start", command=start, width=10)
+funer= Button(app, text="Start", command=go, width=10)
 funer.pack()
 
 app.mainloop()
